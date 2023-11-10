347cf06dafaa05f2e872fbcea1ec4c2b8b98f0c2
ITWILL <ITWILL@WIN-ID1SUN2HKKF>
2023-11-10 19:44:46 +0900

    Java polymorphism

---
+++ 01_Java_polymorphism/src/polymorphism03/TVUser.java
@@ -0,0 +1,30 @@
+package polymorphism03;
+
+import java.util.Arrays;
+
+public class TVUser {
+
+	public static void main(String[] args) {
+		System.out.println("args : " + Arrays.toString(args));
+		
+		BeanFactory factory = new BeanFactory();
+		
+		//SamsungTV, LgTV 사용
+		//TV tv = (TV) factory.getBean("lg");
+		
+		TV tv = (TV) factory.getBean(args[0]);
+		System.out.println("tv : " + tv);
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
+		
+	}
+}
+
+
+
+
+
+
+
